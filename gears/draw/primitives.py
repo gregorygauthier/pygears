ad09085e45de394df8f5f3321b44ca8414845b18
Gregory Gauthier <rjaguar3netgames@yahoo.com>
2011-09-30 17:28:27 -0500

    Add Circle class to primitives

--- gears/draw/primitives.py
+++ gears/draw/primitives.py
@@ -1,6 +1,6 @@
 """PyGears rendering primitives (Rect, Circle, Triangle, etc)."""
 from OpenGL import GL
-
+from math import sin, cos, pi
 
 class Rect(object):
     """A simple, solid color rectangle."""
@@ -19,4 +19,27 @@ class Rect(object):
         GL.glVertex3f(self._width, 0, 0)
         GL.glVertex3f(self._width, self._height, 0)
         GL.glVertex3f(0, self._height, 0)
+        GL.glEnd()
+
+class Circle(object):
+    """This constant determines how many sides the circle's approximating
+    polygon will have when it is rendered"""
+    POLYGON_SIDES = 96
+
+    """A simple, solid color circle."""
+    def __init__(self, radius):
+        """Initialize the Rect with the given width and height.
+        
+        :radius: radius
+        """
+        self._radius = radius
+    
+    def render(self):
+        """Render the circle within the current OpenGL context."""
+        GL.glBegin(GL.GL_TRIANGLE_FAN)
+        GL.glVertex3f(0, 0, 0)
+        for i in range(self.POLYGON_SIDES):
+            theta = 2 * pi * i / self.POLYGON_SIDES
+            GL.glVertex3f(self._radius * cos(theta),
+                self._radius * sin(theta), 0)
         GL.glEnd()
