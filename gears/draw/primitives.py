d373bf54e41c86d0a135634248a4d8420ed53077
Russ Amos <russ.amos@vanderbilt.edu>
2011-10-11 16:05:06 -0500

    Cleaned up comments, whitespace

--- gears/draw/primitives.py
+++ gears/draw/primitives.py
@@ -2,6 +2,7 @@
 from OpenGL import GL
 from math import sin, cos, pi
 
+
 class Rect(object):
     """A simple, solid color rectangle."""
     def __init__(self, width, height):
@@ -21,24 +22,21 @@ class Rect(object):
         GL.glVertex3f(0, self._height, 0)
         GL.glEnd()
 
+
 class Circle(object):
-    """This constant determines how many sides the circle's approximating
-    polygon will have when it is rendered"""
+    """A simple, solid color circle."""
+    # number of sides of the approximating polygon
     POLYGON_SIDES = 96
 
-    """A simple, solid color circle."""
     def __init__(self, radius):
-        """Initialize the Circle with the given radius
-        
-        :radius: radius
-        """
+        """Initialize the Circle with the given radius"""
         self._radius = radius
         theta = [2 * pi * i /self.POLYGON_SIDES for i in \
             range(self.POLYGON_SIDES)]
         self._vertices = [(self._radius * cos(theta[i]),
             self._radius * sin(theta[i]), 0) for i in \
             range(self.POLYGON_SIDES)]
-    
+
     def render(self):
         """Render the circle within the current OpenGL context."""
         GL.glBegin(GL.GL_TRIANGLE_FAN)
@@ -47,22 +45,24 @@ class Circle(object):
             GL.glVertex3f(*self._vertices[i])
         GL.glEnd()
 
+
 class Triangle(object):
-	"""This is a triangle with one vertex at the origin
-	   and the other two vertices at specified locations"""
-	
-	"""Construct the triangle with the origin and the two
-	given ordered pairs as the three vertices.
-	:vertex1:, :vertex2: the vertices
-	"""
-	
-	def __init__(self, vertex1, vertex2):
-		self._vertex1 = (vertex1[0], vertex1[1], 0)
-		self._vertex2 = (vertex2[0], vertex2[1], 0)
-	
-	def render(self):
-		GL.glBegin(GL.GL_TRIANGLES)
-		GL.glVertex3f(0, 0, 0)
-		GL.glVertex3f(*self._vertex1)
-		GL.glVertex3f(*self._vertex2)
-		GL.glEnd()
+    """A simple, solid color triangle."""
+
+    def __init__(self, vertex1, vertex2):
+        """Initialize a triangle.
+
+        Constructs a triangle with the origin and the two
+        given ordered pairs as the three vertices.
+        :vertex1:, :vertex2: the vertices
+
+        """
+        self._vertex1 = (vertex1[0], vertex1[1], 0)
+        self._vertex2 = (vertex2[0], vertex2[1], 0)
+
+    def render(self):
+        GL.glBegin(GL.GL_TRIANGLES)
+        GL.glVertex3f(0, 0, 0)
+        GL.glVertex3f(*self._vertex1)
+        GL.glVertex3f(*self._vertex2)
+        GL.glEnd()
