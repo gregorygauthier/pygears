63275cf6b59ba39b345920980c40ab46c656c6dd
Gregory Gauthier <rjaguar3netgames@yahoo.com>
2011-11-09 11:47:46 -0600

    Fix syntax in pong example.

--- examples/pong/__init__.py
+++ examples/pong/__init__.py
@@ -1,6 +1,6 @@
 from gears import core
 from PySide import QtCore
-def StartState(object):
+class StartState(object):
     def update(dt, app):
         if(app.key_map.get(QtCore.Qt.Key_Space & 0b010) != 0:
             app.states.push(GameState())
