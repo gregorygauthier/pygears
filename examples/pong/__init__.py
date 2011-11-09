afcb7bd9ab4071f487dc127c87c1f2b7536ac7f7
Gregory Gauthier <rjaguar3netgames@yahoo.com>
2011-11-09 11:56:46 -0600

    fix typo in pong example

--- examples/pong/__init__.py
+++ examples/pong/__init__.py
@@ -2,5 +2,5 @@ from gears import core
 from PySide import QtCore
 class StartState(object):
     def update(dt, app):
-        if(app.key_map.get(QtCore.Qt.Key_Space & 0b010) != 0:
+        if(app.key_map.get(QtCore.Qt.Key_Space) & 0b010) != 0:
             app.states.push(GameState())
